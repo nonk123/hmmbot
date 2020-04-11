dfb684f77292d32056121ea001e86647c92f3243
nonk123 <Sergeyii321@gmail.com>
2020-04-11 07:17:40 +0300

    Use the bot for its indended purpose

--- main.py
+++ main.py
@@ -4,10 +4,26 @@ import discord
 
 import os.path
 import json
+import re
 
 class HmmBot(discord.Client):
     async def on_ready(self):
         print("Ready, I guess?")
+
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+
+        respond_to = {
+            r"[Hh]m+[!?]*": "hmm?",
+            r"[Mm]eh\??": "meh",
+            r"[Hh]em\??": "hem",
+            r"[!?]+": "?"
+        }
+
+        for response_regexp, response in respond_to.items():
+            if re.match(response_regexp, message.content):
+                await message.channel.send(response)
 
 def main():
     file = "config.json"
