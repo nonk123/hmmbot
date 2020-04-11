6f6bc05d7655959afd7c3972cdf7231a2f7874d1
nonk123 <Sergeyii321@gmail.com>
2020-04-11 06:55:15 +0300

    Add an actual bot

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+#!/usr/bin/python3
+
+import discord
+
+import os.path
+import json
+
+class HmmBot(discord.Client):
+    async def on_ready(self):
+        print("Ready, I guess?")
+
+def main():
+    file = "config.json"
+
+    if os.path.exists(file):
+        with open(file, "r") as config:
+            HmmBot().run(json.load(config)["token"])
+    else:
+        raise Exception("Config file doesn't exist: %s" % file)
+
 if __name__ == "__main__":
-    print("Hmm?")
+    main()
