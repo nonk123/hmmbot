3207498e05dc666a6a679c57b82c8285210282ba
nonk123 <Sergeyii321@gmail.com>
2020-05-02 15:12:23 +0300

    Decrease the bot's sentience by 30%

--- main.py
+++ main.py
@@ -14,11 +14,8 @@ logging.basicConfig(level=logging.INFO)
 def word(w):
     return "(?:^|{1}){0}(?={1}|$)".format(w, "[,.?!:; ]+")
 
-def ynm(content):
-    return random.choice(["yes.", "no.", "maybe."])
+pick_words = r"(choose|pick|select|decide\s+on)"
 
-pick_words = "(choose|pick|select)"
-pick_choices_pattern = f"(?:{pick_words}.*: *)"
 pick_phrases = [
     "i think i'll go with... {}.",
     "hmm... {}, yes.",
@@ -27,89 +24,94 @@ pick_phrases = [
     "repick.",
     "meh.",
     "hmm?",
-    "your question sucks."
+    "nope."
 ]
 
-def pick(content):
-    split = re.split(pick_choices_pattern, content, 2)
-    choices = split[-1]
+def pick(match):
+    choices = match.group("choices")
 
-    if len(split) < 2 or not choices:
+    if not choices:
         return "and what should i pick? (tm)"
 
     if "|" in choices:
         choices = re.split(r"\s*\|\s*", choices)
     elif '"' in choices:
         choices = [s[1:-1] for s in re.findall(r'".*?"', choices)]
+    elif "," in choices:
+        choices = re.split(r",\s*", choices)
     else:
-        choices = re.split(",\s*|\s+", choices)
+        choices = re.split(r"\s+", choices)
 
-    choice = "`%s`" % random.choice(choices)
-    return random.choice(pick_phrases).format(choice)
-
-modals = "((do|did|can|could|have|has|had|should)(n't)?|shall)"
-pronouns = "(I|you|he|she|it|we|they)"
+    return random.choice(pick_phrases).format(f"__{random.choice(choices)}__")
 
 respond_to = {
-    fr"(^|.+,? +){pick_words}": pick,
-    fr"{modals} +{pronouns}|you +{modals}": ynm,
+    fr"^bot(,\s*|,?\s+){pick_words}[^.?!]*:\s*(?P<choices>.*)$": pick,
     r"^test$": "test?",
-    r"^ping$": "pong!",
-    r"^pong$": "ping?",
-    r"^well([.?]*$|hr?m+$)": "well?",
+    r"^well((,?\s+hr?m+)|[.?]*)$": "well?",
     r"^hr?m+\??$": "hmm?",
-    word("me+h"): "meh.",
-    word("he+m"): "hem.",
-    r"^[ -/:-@\[-`{-~]+$": "?",
-    fr"{word('brb')}|{word('gtg')}|^afk(, *| +)eating|^back$": "ok.",
-    r"^ready|ready$": "no, you're not.",
-    word("(fuck|fric[ck]) +you"): "no u",
+    r"^meh$": "meh.",
+    r"^hem$": "hem.",
+    r"^[\s -/:-@\[-`{-~]+$": "?",
+    r"^ready$": "no, you're not.",
     r"^\*\*.+\*\*$": "hmm?"
 }
 
-def parse_vgs():
-    from pyquery import PyQuery
+vgs_lines = {}
 
-    vgs = {}
+def init_vgs():
+    from pyquery import PyQuery
 
     pq = PyQuery(url="http://wiki.theexiled.pwnageservers.com/Tribes:_Ascend/Voice_Game_System")
 
     for line in pq("{0}:not(:parent), {0} > dl > dd".format("h2 + dl > dd")).items():
         split = line.text().split("] ", 2)
 
-        shortcut = split[0][1:]
-        command = split[1].rstrip()
+        shortcut = split[0][1:].strip()
+        command = split[1].strip()
 
-        vgs[word(shortcut)] = command
+        vgs_lines[shortcut] = command
 
-    return vgs
+    def vgs(match):
+        responses = []
 
-respond_to.update(parse_vgs())
+        for shortcut in re.findall(match.re, match.string):
+            shortcut = shortcut.upper()
+
+            if shortcut in vgs_lines:
+                responses.append(vgs_lines[shortcut])
+
+        return " ".join(responses)
+
+    pattern = "|".join(shortcut for shortcut, _ in vgs_lines.items())
+
+    respond_to[fr"({pattern}(?:\s+|$))+"] = vgs
+
+init_vgs()
 
 class HmmBot(discord.Client):
     async def on_ready(self):
         respond_to[fr"<@!?{self.user.id}>"] = "yes?"
-        respond_to[fr"<@!?(\d+(?<!{self.user.id}))>"] = "hmm?"
 
         print("Ready, I guess?")
 
     def respond(self, content):
-        for response_regexp, response in respond_to.items():
-            for match in re.findall(response_regexp, content, re.IGNORECASE):
+        for pattern, response in respond_to.items():
+            match = re.match(pattern, content, re.IGNORECASE)
+
+            if match:
                 if callable(response):
-                    yield response(content)
-                    return
+                    return response(match)
                 else:
-                    yield response
+                    return response
 
     async def on_message(self, message):
         if message.author.id == self.user.id:
             return
 
-        responses = list(self.respond(message.content))
+        response = self.respond(message.content)
 
-        if responses:
-            await message.channel.send(" ".join(responses))
+        if response:
+            await message.channel.send(response)
 
 def main():
     file = "config.json"
